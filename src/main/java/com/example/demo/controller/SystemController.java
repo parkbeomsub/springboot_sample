386619ccb2009b1cbe236243c33b3bc7068dad04
root <root@pbs-5f.tossmobile.team>
2024-10-04 12:09:14 +0900

    test

---
+++ src/main/java/com/example/demo/controller/SystemController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SystemController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+}
